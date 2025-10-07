812ef25da283a4792299dbfc8742c99070bb7c8e
jacekm91fidzinski-cmd <jacekm91fidzinski@gmail.com>
2025-10-08 01:26:56 +0200

    removed empty line R4 in Main

--- src/main/java/core/basesyntax/Machine.java
+++ src/main/java/core/basesyntax/Machine.java
@@ -1,9 +1,11 @@
 package core.basesyntax;
 public abstract class Machine {
     /**
-     * Starts the machine's work process.*/
+     * Starts the machine's work process.
+     */
     public abstract void doWork();
     /**
-     * Stops the machine's work process.*/
+     * Stops the machine's work process.
+     */
     public abstract void stopWork();
 }
