f7a08950204a3a8b9850acea12e92a361e96d7fe
jacekm91fidzinski-cmd <jacekm91fidzinski@gmail.com>
2025-10-08 00:14:32 +0200

    removed [], Class "Machine" -added newline at the end, separated start/stop in two loops, added commentary in "Machine" class

--- src/main/java/core/basesyntax/Machine.java
+++ src/main/java/core/basesyntax/Machine.java
@@ -1,6 +1,10 @@
 package core.basesyntax;
 
 public abstract class Machine {
-    public abstract void doWork(); //Starts the machine's work process
-    public abstract void stopWork(); // Stops the machine's work process
+    /**Starts the machine's work process.
+     */
+    public abstract void doWork();
+    /**Stops the machine's work process.
+     */
+    public abstract void stopWork(); /* Stops the machine's work process*/
 }
